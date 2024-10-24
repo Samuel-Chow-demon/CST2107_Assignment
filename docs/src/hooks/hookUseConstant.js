f59f72787f79ec883480fe1185c30119ba7cc5e4
Samuel_Chow <demondemontcchow@gmail.com>
2024-10-23 19:00:53 -0700

    Add docs folder
    copy Assign2 content into docs for deploy

---
+++ docs/src/hooks/hookUseConstant.js
@@ -0,0 +1,16 @@
+
+const LOG_TYPE = {
+    error : "Error",
+    warning : "Warning",
+    log : "Log",
+    debug : "Debug",
+    info : "Info"
+}
+
+const LOG_FORM_OBJ = {
+    scope : "",
+    type : LOG_TYPE.log,
+    message : ""
+}
+
+export {LOG_TYPE, LOG_FORM_OBJ};
